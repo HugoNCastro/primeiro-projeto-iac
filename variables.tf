7a935d1fa12e7da8d08774ce50e82f672d4cbf16
HugoNCastro <hugonaaman@gmail.com>
2024-05-08 21:45:37 -0300

    Cria módulo do cloudfront

--- variables.tf
+++ variables.tf
@@ -1,11 +1,5 @@
-variable "org_name" {
-  type        = string
-  default     = "hnc-devops"
-  description = "Nome do bucket"
-}
-
-variable tf_profile {
-  type  = string
-  default = ""
+variable "tf_profile" {
+  type      = string
+  default   = ""
   sensitive = true
 }
